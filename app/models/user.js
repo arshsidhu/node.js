11d5caea3ecab2222e86cfaec1a9aa2ca8fd6f48
Arshdeep Sidhu <arsh@Arshdeeps-MacBook-Pro.local>
2018-07-05 21:48:31 -0400

    added signup frontend and set up userschema

--- app/models/user.js
+++ app/models/user.js
@@ -1,1 +1,39 @@
 //User model
+
+//Load things we need
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
+
+//Define user schema
+var userSchema = mongoose.Schema({
+	local:{
+		email:String,
+		password:String,
+	},
+	facebook:{
+		id:String,
+		token:String,
+		name:String,
+		email:String,
+	},
+	twitter:{
+		id:String,
+		token:String,
+		name:String,
+		email:String,
+	}
+});
+
+//Methods
+//Gen a hash
+userSchema.methods.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+//checking if password is valid
+userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+};
+
+//create model for users and send to app
+module.exports = mongoose.model('User', userSchema);
